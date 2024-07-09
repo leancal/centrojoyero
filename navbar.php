e52b1f9d2072159335b299f21c2239ce747036c7
Leandro Calvet <leandrocalvet14@gmail.com>
2024-07-08 23:23:30 -0300

    update

--- navbar.php
+++ navbar.php
@@ -2,20 +2,20 @@
 <nav class="navbar">
     <ul class="menu">
         <li>
-            <a href="Anillos_Oro_14k.php">ANILLOS</a>
+            <a href="#">ANILLOS &#9662;</a> <!-- Flecha hacia abajo -->
             <div class="sub-menu">
                 <ul>
                     <li>
                         <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_552_Joyeria_Fina_FourteenKarat.png" alt="Anillos" class="sub-menu-img">
                     </li>
-                    <li><a href="#">Anillos de Compromiso</a></li>
-                    <li><a href="#">Solitarios</a></li>
-                    <li><a href="#">Churumbelas</a></li>
+                    <li><a href="./Anillos_compromiso.php">Anillos de Compromiso</a></li>
+                    <li><a href="./Anillos_solitarios.php">Solitarios</a></li>
+                    <li><a href="./Anillos_churumbelas.php">Churumbelas</a></li>
                 </ul>
             </div>
         </li>
         <li>
-            <a href="#">ARETES</a>
+            <a href="#">ARETES &#9662;</a> <!-- Flecha hacia abajo -->
             <div class="sub-menu">
                 <ul>
                     <li>

c33a4d1259b7f5b9ef4e22376a2b61d7b3dfcf74
Leandro Calvet <leandrocalvet14@gmail.com>
2024-07-09 13:14:25 -0300

    update

--- Anillos_Compromiso.php
+++ Anillos_Compromiso.php
@@ -7,34 +7,262 @@ $content = '<h3 style="text-align: center;" > Anillos de Compromiso</h3>
                         <li class="grid-sizer"></li><!-- for Masonry column width -->
                        
                         <li>
-                            <figure>
-                                  <figcaption><h3>Corazón </h3></figcaption> 
-                                <img src="ImgFourteenKarat/Anillos/Anillos_Compromiso/Corazon_14k_172gr.jpg" alt="img01"/>
-                            </figure>
-                        </li>
-                         <li>
-                            <figure>
-                                  <figcaption><h3>Piedra Negra y Blanca </h3></figcaption> 
-                                <img src="ImgFourteenKarat/Anillos/Anillos_Compromiso/Piedra_Negra_Blanca_14k_2_0gr.jpg" alt="img02"/>
-                            </figure>
-                        </li>                      
+                <figure>
+                    <figcaption><h3>Anello Cocktail</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/anello-cocktail-l4946.png" alt="anello cocktail"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anello del Tempo</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/anello-del-tempo-w0535.png" alt="anello del tempo"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anello GIADA Pagoda</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anello-GIADA-Pagoda-R5765-FB.png" alt="anello giada pagoda"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 124</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_124_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 124"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 131</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_131_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 131"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 150</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_150_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 150"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 151</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_151_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 151"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 152</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_152_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 152"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 153</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_153_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 153"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 158</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_158_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 158"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 15</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_15_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 15"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 166</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_166_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 166"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 167</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_167_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 167"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 168</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_168_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 168"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 172</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_172_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 172"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 177</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_177_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 177"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 190</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_190_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 190"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 218</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_218_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 218"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 259</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_259_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 259"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 268</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_268_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 268"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 285</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_285_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 285"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 312</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_312_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 312"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 316</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_316_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 316"/>
+                </figure>
+            </li>       
                     </ul>
                 </section><!-- // grid-wrap -->
                 <section class="slideshow">
                     <ul>
                        
                          <li>
-                            <figure>
-                                  <figcaption><h3>Corazón </h3></figcaption> 
-                                <img src="ImgFourteenKarat/Anillos/Anillos_Compromiso/Corazon_14k_172gr.jpg" alt="img01"/>
-                            </figure>
-                        </li>
-                         <li>
-                            <figure>
-                                  <figcaption><h3>Piedra Negra y Blanca </h3></figcaption> 
-                                <img src="ImgFourteenKarat/Anillos/Anillos_Compromiso/Piedra_Negra_Blanca_14k_2_0gr.jpg" alt="img02"/>
-                            </figure>
-                        </li>  
+                <figure>
+                    <figcaption><h3>Anello Cocktail</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/anello-cocktail-l4946.png" alt="anello cocktail"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anello del Tempo</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/anello-del-tempo-w0535.png" alt="anello del tempo"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anello GIADA Pagoda</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anello-GIADA-Pagoda-R5765-FB.png" alt="anello giada pagoda"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 124</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_124_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 124"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 131</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_131_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 131"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 150</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_150_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 150"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 151</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_151_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 151"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 152</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_152_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 152"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 153</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_153_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 153"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 158</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_158_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 158"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 15</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_15_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 15"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 166</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_166_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 166"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 167</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_167_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 167"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 168</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_168_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 168"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 172</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_172_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 172"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 177</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_177_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 177"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 190</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_190_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 190"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 218</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_218_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 218"/>
+                </figure>
+            </li>
+            <li>
+                <figure>
+                    <figcaption><h3>Anillo de Oro 14k FKD 259</h3></figcaption>
+                    <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_259_Joyeria_Fina_FourteenKarat.png" alt="anillo oro 14k 259"/>
+                </figure>
+            </li> 
                                             
 
 

fcb378ac8aed5defee5763e12b47ab9fae7574c9
Leandro Calvet <leandrocalvet14@gmail.com>
2024-07-09 12:36:04 -0300

    update

--- navbar.php
+++ navbar.php
@@ -1,38 +1,53 @@
 <!-- navbar.php -->
+
+
+
 <nav class="navbar">
-    <ul class="menu">
-        <li>
-            <a href="#">ANILLOS &#9662;</a> <!-- Flecha hacia abajo -->
-            <div class="sub-menu">
-                <ul>
-                    <li>
-                        <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_552_Joyeria_Fina_FourteenKarat.png" alt="Anillos" class="sub-menu-img">
-                    </li>
-                    <li><a href="./Anillos_compromiso.php">Anillos de Compromiso</a></li>
-                    <li><a href="./Anillos_solitarios.php">Solitarios</a></li>
-                    <li><a href="./Anillos_churumbelas.php">Churumbelas</a></li>
-                </ul>
-            </div>
-        </li>
-        <li>
-            <a href="#">ARETES &#9662;</a> <!-- Flecha hacia abajo -->
-            <div class="sub-menu">
-                <ul>
-                    <li>
-                        <img src="JoyeriaOro/Aretes/4.png" alt="Aretes" class="sub-menu-img">
-                    </li>
-                    <li><a href="#">Huggies</a></li>
-                    <li><a href="#">Violadores</a></li>
-                    <li><a href="#">Broqueles</a></li>
-                </ul>
-            </div>
-        </li>
-        <li><a href="#">ARRACADAS</a></li>
-        <li><a href="#">ESCLAVAS</a></li>
-        <li><a href="#">NATURALEZA</a></li>
-        <li><a href="#">DIJES</a></li>
-        <li><a href="#">MÁS</a></li>
-        <li><a href="Contacto.php">CONTÁCTANOS</a></li>
-        <li><a href="Nosotros.php">NOSOTROS</a></li>
-    </ul>
+  <ul class="menu">
+    <li>
+      <a href="#">ANILLOS &#9662;</a> <!-- Flecha hacia abajo -->
+      <div class="sub-menu">
+        <ul>
+
+          <li>
+            <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14k_FKD_552_Joyeria_Fina_FourteenKarat.png" alt="Anillos" class="sub-menu-img">
+            <a href="./Anillos_compromiso.php">Anillos de Compromiso</a>
+          </li>
+          <li>
+            <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14K_FKTD_77_Joyeria_Fina_FourteenKarat.png" alt="Anillos" class="sub-menu-img">
+            <a href="./Anillos_solitarios.php">Solitarios</a>
+          </li>
+          <li>
+            <img src="JoyeriaOro/Anillos/Anillo_de_Oro_14K_FKTD_77_Joyeria_Fina_FourteenKarat.png" alt="Anillos" class="sub-menu-img">
+            <a href="./Anillos_churumbelas.php">Churumbelas</a>
+          </li>
+        </ul>
+      </div>
+    </li>
+    <li>
+      <a href="#">ARETES &#9662;</a> <!-- Flecha hacia abajo -->
+      <div class="sub-menu">
+        <ul>
+
+          <li>
+            <img src="JoyeriaOro/Aretes/4.png" alt="Aretes" class="sub-menu-img">
+            <a href="#">Huggies</a>
+          </li>
+          <li>
+          <img src="JoyeriaOro/Aretes/4.png" alt="Aretes" class="sub-menu-img">
+          <a href="#">Violadores</a></li>
+          <li>
+            <img src="JoyeriaOro/Aretes/4.png" alt="Aretes" class="sub-menu-img">
+            <a href="#">Broqueles</a></li>
+        </ul>
+      </div>
+    </li>
+    <li><a href="#">ARRACADAS</a></li>
+    <li><a href="#">ESCLAVAS</a></li>
+    <li><a href="#">NATURALEZA</a></li>
+    <li><a href="#">DIJES</a></li>
+    <li><a href="#">MÁS</a></li>
+    <li><a href="Contacto.php">CONTÁCTANOS</a></li>
+    <li><a href="Nosotros.php">NOSOTROS</a></li>
+  </ul>
 </nav>
